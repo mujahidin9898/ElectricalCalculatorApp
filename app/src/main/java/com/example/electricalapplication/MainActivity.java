5f759b0e8d6acfa8abacdd959b89e549f29a1133
mujahidin9898 <136341195+mujahidin9898@users.noreply.github.com>
2023-06-14 03:36:32 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/electricalapplication/MainActivity.java
+++ app/src/main/java/com/example/electricalapplication/MainActivity.java
@@ -26,29 +26,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the views
+        
         editTextUnits = findViewById(R.id.editTextUnits);
         editTextRebate = findViewById(R.id.editTextRebate);
         buttonCalculate = findViewById(R.id.buttonCalculate);
         textViewResult = findViewById(R.id.textViewResult);
         buttonAbout = findViewById(R.id.buttonAbout);
 
-        // Set a click listener for the Calculate button
+       
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Retrieve the input values
+               
                 int units = Integer.parseInt(editTextUnits.getText().toString());
                 double rebate = Double.parseDouble(editTextRebate.getText().toString()) / 100;
 
-                // Calculate the total charges based on the given rate table and units used
+                
                 double totalCharges = calculateTotalCharges(units);
 
-                // Deduct the rebate percentage from the total charges
+               
                 double rebateAmount = totalCharges * rebate;
                 double finalCost = totalCharges - rebateAmount;
 
-                // Display the final cost to the user
+                
                 textViewResult.setText("Final Cost: RM " + String.format("%.2f", finalCost));
             }
         });
@@ -56,25 +56,24 @@ public class MainActivity extends AppCompatActivity {
         buttonAbout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Open the AboutActivity
+                
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-    // Function to perform an action from the AboutActivity
+    
     public void performActionFromAbout() {
         Toast.makeText(this, "Action from AboutActivity performed", Toast.LENGTH_SHORT).show();
-        // Add your desired functionality here
-        // This function can be called from the AboutActivity or any other activity
+        
+        
     }
 
-    // Helper method to calculate the total charges based on the given rate table and units used
+    
     private double calculateTotalCharges(int units) {
         double totalCharges = 0.0;
 
-        // Calculate charges for each block
         if (units <= 200) {
             totalCharges = units * 0.218;
         } else if (units <= 300) {
